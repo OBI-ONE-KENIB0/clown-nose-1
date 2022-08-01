1cb2d98e98b00d9ebb89de438efd17f9c6cb2193
OBI-ONE-KENIB0 <97593769+OBI-ONE-KENIB0@users.noreply.github.com>
2022-08-01 15:26:57 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(350,350);
+canavs.center();
+video=createCapture(VIDEO);
+video.size(350,350);
+video.hide();
+}
+
+function draw(){
+
+}
